f6424a318c85848058010e50773a2548e5bb1813
David Hu <davidhu314@gmail.com>
2017-03-23 16:29:14 -0700

    create session controller and apssed session specs

--- spec/controllers/api/sessions_controller_spec.rb
+++ spec/controllers/api/sessions_controller_spec.rb
@@ -6,7 +6,8 @@ rescue
   Api::SessionsController = nil
 end
 
-RSpec.describe Api::SessionController, type: :controller do
+RSpec.describe Api::SessionsController, type: :controller do
+  let!(:user) { User.create({username: "robo_gigster", password: "not_password" }) }
 
   describe 'login with invalid credentials' do
     it 'should render error if user does not exist' do
@@ -23,16 +24,14 @@ RSpec.describe Api::SessionController, type: :controller do
   describe 'login with valid credentials' do
     it 'login the user' do
       post :create, params: { user: { username: 'robo_gigster', password: 'not_password' } }
+      user = User.find_by(username: 'robo_gigster')
       expect(session[:session_token]).to eq(user.session_token)
     end
   end
 
   describe 'logout' do
-    before(:each) do
-      post :create, params: { user: { username: 'robo_gigster', password: 'not_password' } }
-    end
-
     it 'logs out the user' do
+      post :create, params: { user: { username: 'robo_gigster', password: 'not_password' } }
       delete :destroy
       expect(session[:session_token]).to be_nil
     end
