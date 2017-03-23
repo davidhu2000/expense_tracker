59d86f80266d60d78c6871745249f7a07af4c8a3
David Hu <davidhu314@gmail.com>
2017-03-23 15:03:07 -0700

    add user controller tests

--- spec/controllers/api/session_controller_spec.rb
+++ spec/controllers/api/session_controller_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Api::SessionController, type: :controller do
 
   let!(:user) { User.create(username: "robo_gigster", password: "not_password") }
 
-  context 'login with invalid credentials' do
+  describe 'login with invalid credentials' do
     it 'should render error if user does not exist' do
       post :create, user: { username: 'robo_other', password: 'password' }
       expect(response.status).to be 401
@@ -22,14 +22,14 @@ RSpec.describe Api::SessionController, type: :controller do
     end
   end
 
-  context 'login with valid credentials' do
+  describe 'login with valid credentials' do
     it 'login the user' do
       post :create, user: { username: 'robo_gigster', password: 'not_password' }
       expect(session[:session_token]).to eq(user.session_token)
     end
   end
 
-  context 'logout' do
+  describe 'logout' do
     before(:each) do
       post :create, user: { username: 'robo_gigster', password: 'not_password' }
     end
--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe Api::UsersController, type: :controller do
+begin
+  UsersController
+rescue
+  UsersController = nil
+end
+
+RSpec.describe UsersController, :type => :controller do
+
+  let!(:valid_user) { User.new(username: "robo_gigster", password: "not_password") }
+  let!(:invalid_user) { User.new(username: "bad_robot", password: 'short') }
+
+  describe 'signup with invalid credentials' do
+    it 'should render error if password is too short' do
+      post :create, user: { username: 'bad_robot', password: 'short' }
+      expect(response.status).to be 422
+    end
+
+    it 'should render error if password is missing' do
+      post :create, user: { username: 'robo_gigster', password: '' }
+      expect(response.status).to be 422
+    end
+  end
+
+  describe 'signup with valid credentials' do
+    before(:each) do
+      post :create, user: { username: 'robo_gigster', password: 'not_password' }
+      @user = User.find_by_credentials('robo_gigster', 'not_password')
+    end
+
+    it 'user exists in database' do
+      expect(@user).not_to be_nil
+    end
+
+    it 'login the user' do
+      expect(session[:session_token]).to eq(@user.session_token)
+    end
+  end
 
 end
