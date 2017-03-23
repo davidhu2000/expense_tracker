4aaf8d67488c0073a9c36a6434a672d7e9fa8693
David Hu <davidhu314@gmail.com>
2017-03-23 14:22:51 -0700

    wrote tests for user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+begin
+  User
+rescue
+  User = nil
+end
+
+RSpec.describe User, type: :model do
+
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:session_token) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_length_of(:password).is_at_least(6) }
+  it { should have_many(:expenses) }
+
+  describe 'encrypting password' do
+    it 'uses BCrypt to encryp the password' do
+      expect(BCrypt::Password).to receive(:create)
+      User.new(username: 'robo_gigster', password: 'not_password')
+    end
+
+    it 'should not save the password to database' do
+      User.create!(username: 'robo_gigster', password: 'not_password')
+      user = User.find_by(username: 'robo_gigster')
+      expect(user.password).not_to be ('not_password')
+    end
+  end
+
+end
