d6ed349786649903204f6078ff0f5893ffdd5311
David Hu <davidhu314@gmail.com>
2017-03-23 14:56:04 -0700

    add session controller specs

---
+++ spec/controllers/api/session_controller_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+begin
+  SessionsController
+rescue
+  SessionsController = nil
+end
+
+RSpec.describe Api::SessionController, type: :controller do
+
+  let!(:user) { User.create(username: "robo_gigster", password: "not_password") }
+
+  context 'login with invalid credentials' do
+    it 'should render error if user does not exist' do
+      post :create, user: { username: 'robo_other', password: 'password' }
+      expect(response.status).to be 401
+    end
+
+    it 'should render error if password is incorrect' do
+      post :create, user: { username: 'robo_gigster', password: 'password' }
+      expect(response.status).to be 401
+    end
+  end
+
+  context 'login with valid credentials' do
+    it 'login the user' do
+      post :create, user: { username: 'robo_gigster', password: 'not_password' }
+      expect(session[:session_token]).to eq(user.session_token)
+    end
+  end
+
+  context 'logout' do
+    before(:each) do
+      post :create, user: { username: 'robo_gigster', password: 'not_password' }
+    end
+
+    it 'logs out the user' do
+      delete :destroy
+      expect(session[:session_token]).to be_nil
+    end
+  end
+end
