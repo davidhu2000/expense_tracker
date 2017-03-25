231e8f989f57751c9289e96e24e60c34ece58594
David Hu <davidhu314@gmail.com>
2017-03-25 14:05:59 -0700

    add function to create weekly totals, add weekly_report_item to render total for each week

--- frontend/components/reports/weekly_report.jsx
+++ frontend/components/reports/weekly_report.jsx
@@ -1,13 +1,69 @@
 import React from 'react';
+import moment from 'moment';
+import { values } from 'lodash';
+import WeeklyReportItem from './weekly_report_item';
 
 class WeeklyReport extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+
+    this.state = {
+      weekTotals: {}
+    }
+  }
+
+  componentDidMount() {
+    this.createReport();
+  }
+
+  createReport() {
+    let weekTotals = {};
+
+    values(this.props.expenses).forEach( expense => {
+
+      let date = moment(expense.expense_date).format("MMDDYYYY")
+      let weekNumber = moment(date, "MMDDYYYY").isoWeek();
+      let year = moment(date, 'MMDDYYYY').year();
+
+      let key = year + weekNumber / 100;
+
+      if(weekTotals[key]) {
+        weekTotals[key] += expense.amount;
+      } else {
+        weekTotals[key] = expense.amount;
+      }
+    });
+
+    this.setState({ weekTotals });
+  }
+
+  renderReport() {
+    return Object.keys(this.state.weekTotals).sort().map( dateKey => {
+      return (
+        <WeeklyReportItem
+          key={dateKey}
+          dateKey={dateKey}
+          totalExpense={this.state.weekTotals[dateKey]} />
+      );
+    })
   }
 
   render() {
+
     return (
-      <div>Weekly Report</div>
+      <div>
+        <div>Weekly Report</div>
+        <table>
+          <tbody>
+            <tr>
+              <th>Year</th>
+              <th>Week Number</th>
+              <th>Total Expense</th>
+            </tr>
+            {this.renderReport()}
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
